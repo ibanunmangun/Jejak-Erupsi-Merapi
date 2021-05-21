3d754a711f35874c04d9c30923d117805c40138f
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-05-21 18:25:34 +0700

    Add files via upload

---
+++ data/Meancoordinates_3.js
@@ -0,0 +1,8 @@
+var json_Meancoordinates_3 = {
+"type": "FeatureCollection",
+"name": "Meancoordinates_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "MEAN_X": 110.42806412727273, "MEAN_Y": -7.6162391909090896 }, "geometry": { "type": "Point", "coordinates": [ 110.428064127272734, -7.61623919090909 ] } }
+]
+}
---
+++ data/PointofInterest_2.js
@@ -0,0 +1,18 @@
+var json_PointofInterest_2 = {
+"type": "FeatureCollection",
+"name": "PointofInterest_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Merapi Volcano Museum", "desc": "Merapi Volcano Museum, Jl. Kaliurang No.Km, Banteng, Hargobinangun, Pakem, Sleman Regency, Special Region of Yogyakarta 55582, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.424332347460066, -7.615915904273341 ] } },
+{ "type": "Feature", "properties": { "name": "Jadah Tempe Mbah Carik Pertama", "desc": "Jadah Tempe Mbah Carik Pertama, Jl. Astomulyo No.50, Kaliurang, Hargobinangun, Pakem, Sleman Regency, Special Region of Yogyakarta 55582, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.4269732, -7.6038162 ] } },
+{ "type": "Feature", "properties": { "name": "Ullen Sentalu Museum", "desc": "Museum Ullen Sentalu, Jl. Boyong No.KM 25, Kaliurang, Hargobinangun, Pakem, Sleman Regency, Special Region of Yogyakarta 55582, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.4233959, -7.5978656 ] } },
+{ "type": "Feature", "properties": { "name": "Sate Tongseng Mbah Ganis", "desc": "Sate Tongseng Mbah Ganis, Kaliurang, Hargobinangun, Pakem, Sleman Regency, Special Region of Yogyakarta 55582, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.4309534, -7.5942343 ] } },
+{ "type": "Feature", "properties": { "name": "Dusun Kinahrejo Rumah Mbah Marijan", "desc": "Dusun Kinahrejo Rumah Mbah Marijan, Kinarejo, Umbulharjo, Cangkringan, Sleman Regency, Special Region of Yogyakarta 55583, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.4439456, -7.5831218 ] } },
+{ "type": "Feature", "properties": { "name": "Plunyon Kalikuning", "desc": "Plunyon Kalikuning, CangkringanCangkringanCangkringanCangkringan, Kedungsriti, Sidorejo, Umbulharjo, Kec. Cangkringan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55583, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.435459, -7.603718 ] } },
+{ "type": "Feature", "properties": { "name": "Ledok Sambi", "desc": "Ledok Sambi, Kaliurang St No.KM. 19, 2, Area Sawah, Pakembinangun, Pakem, Sleman Regency, Special Region of Yogyakarta 55582, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.4280998, -7.6470943 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Al Amin Kaliurang", "desc": null, "X": null, "Y": null, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.430112, -7.598463 ] } },
+{ "type": "Feature", "properties": { "name": "Pos Polisi Kaliurang", "desc": null, "X": null, "Y": null, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.426958, -7.603343 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Jiwa Ghrasia", "desc": null, "X": null, "Y": null, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.421475, -7.661859 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Panti Nugroho", "desc": null, "X": null, "Y": null, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.417179, -7.668781 ] } }
+]
+}
